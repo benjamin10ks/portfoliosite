c85f246dcdeb11f2ffa19a4e8c747c00ae3badd1
unknown <bjkmanning@gmail.com>
2025-04-02 18:14:20 -0600

    image optimization

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
-import Head from "next/head";
 import ProjectCard from "./components/ProjectCard";
 import Particles from "./components/Particles";
+import Image from "next/image";
+
+export const metadata = {
+  title: 'Brendan Kelly',
+  description: 'A simple portfolio page built with Next.js',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
 
 export default function Home() {
   return (
     <div className="relative bg-gradient-to-t from-black to-gray-800 ">
       <Particles count={80} />
-      <Head>
-        <title>Brendan Kelly</title>
-        <meta
-          name="description"
-          content="A simple portfolio page built with Next.js"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <div className="min-h-screen flex items-center justify-center p-4">
         <div className="flex flex-row ">
           <div className="text-left mr-[120px] items-center pt-8 pb-8">
@@ -32,28 +32,27 @@ export default function Home() {
             </section>
           </div>
           <div className="flex items-center">
-            <img src="globe.svg" alt="placeholder" className="h-[400px] w-[400px]" />
+            <Image src="/globe.svg" alt="globe" width={400} height={400} />
           </div>
         </div>
       </div>
-
 
       <section className="flex flex-col gap-8 justify-center items-center justify-evenly">
         <p className="font-bold text-[40px]">Projects</p>
         <ProjectCard
           title="Placeholder"
           description="A brief description of this project."
-          technologies={["images/react_logo.png", "next.svg"]}
+          technologies={["/images/react_logo.png", "/next.svg"]}
         />
         <ProjectCard
           title="Placeholder"
           description="A brief description of this project."
-          technologies={["images/typescript.png", "next.svg"]}
+          technologies={["/images/typescript.png", "/next.svg"]}
         />
         <ProjectCard
           title="Placeholder"
           description="A brief description of this project."
-          technologies={["images/unitylogo.png", "images/Csharp_Logo.png"]}
+          technologies={["/images/unitylogo.png", "/images/Csharp_Logo.png"]}
         />
       </section>
 
